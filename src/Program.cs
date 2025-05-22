6d3a8f4e83555ed1337d820a27e645a9d9f55782
joseyvanaarsen <joseyvanaarsen@gmail.com>
2025-05-22 13:29:39 +0200

    Removed unnessecary comments

--- src/Program.cs
+++ src/Program.cs
@@ -17,9 +17,4 @@ public class Composers
 {
     public static Func<A, C> Compose<A, B, C>(Func<A, B> func1, Func<B, C> func2) => p => func2(func1(p));
     public static Func<int, int> ComposeN(Func<int, int> f, int n) => n == 0 ? p => p : p => ComposeN(f, n - 1)(f(p));
-    // {
-    //     if (n == 0) return p => p;
-    //     if (n == 1) return f;
-    //     return p => ComposeN(f, n - 1)(f(p));
-    // }
 }
